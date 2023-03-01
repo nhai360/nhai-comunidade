35c26f57920f66b240c3fdc3f909cb04747da285
Giovanna <eugiovannasouza@gmail.com>
2023-03-01 11:25:24 -0300

    feat: add login page

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import type { AppProps } from "next/app";
+import { globalStyles } from "@/../stitches.config";
+
+export default function App({ Component, pageProps }: AppProps) {
+  globalStyles();
+
+  return <Component {...pageProps} />;
+}
