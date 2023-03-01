49d0e34e96e8f47add76c03af3e587b2c95b6aa1
Giovanna <eugiovannasouza@gmail.com>
2023-03-01 15:18:18 -0300

    feat: add label component

---
+++ src/ui/Label/Label.ts
@@ -0,0 +1,8 @@
+import { styled } from "@/../stitches.config";
+
+export const Label = styled("label", {
+  lineHeight: "16px",
+  fontSize: "$body3",
+  fontWeight: 600,
+  fontFamily: "$heading",
+});
