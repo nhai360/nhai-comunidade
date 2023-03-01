475f799f87c20907d827dad3d000013fa757679d
Giovanna <eugiovannasouza@gmail.com>
2023-03-01 15:17:55 -0300

    feat: add button component

---
+++ src/ui/Button/Button.ts
@@ -0,0 +1,69 @@
+import { styled } from "@/../stitches.config";
+
+export const Button = styled("button", {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+
+  columnGap: "$2",
+  borderRadius: "$medium",
+  border: 0,
+
+  fontFamily: "$body",
+  fontSize: "$body2",
+  fontWeight: 700,
+  lineHeight: "150%",
+  letterSpacing: "0.04rem",
+
+  transition: "all 0.2s",
+
+  "&:disabled": {
+    background: "$neutral500",
+    color: "$textPrimary",
+    cursor: "not-allowed",
+  },
+
+  variants: {
+    variant: {
+      primary: {
+        color: "$neutral100",
+        background: "$blueMedium",
+
+        "&:not(:disabled):hover": {
+          background: "$blueDark",
+        },
+      },
+      outline: {
+        background: "transparent",
+        color: "$textPrimary",
+        border: "2px solid $blueMedium",
+      },
+      transparent: {
+        background: "transparent",
+        color: "$blueDark",
+      },
+    },
+    size: {
+      large: {
+        padding: "$4 $6",
+      },
+      medium: {
+        padding: "$3 $4",
+      },
+      small: {
+        padding: "$2 $3",
+      },
+    },
+    fullWidth: {
+      true: {
+        width: "100%",
+      },
+    },
+  },
+
+  defaultVariants: {
+    variant: "primary",
+    size: "large",
+    fullWidth: false,
+  },
+});
