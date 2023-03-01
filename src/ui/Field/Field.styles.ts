d1f44754a3692ebf3867e3f7d5bbfeb3dc9c5240
Giovanna <eugiovannasouza@gmail.com>
2023-03-01 15:18:11 -0300

    feat: add field component

---
+++ src/ui/Field/Field.styles.ts
@@ -0,0 +1,15 @@
+import { styled } from "@/../stitches.config";
+
+export const Container = styled("fieldset", {
+  border: 0,
+
+  display: "flex",
+  flexDirection: "column",
+  gap: "$3",
+});
+
+export const LabelContainer = styled("div", {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+});
---
+++ src/ui/Field/Field.tsx
@@ -0,0 +1,54 @@
+import {
+  ForwardRefRenderFunction,
+  forwardRef,
+  InputHTMLAttributes,
+} from "react";
+
+import { Input, Label, Typography } from "@/ui";
+
+import * as S from "./Field.styles";
+
+type Props = {
+  label?: string;
+  helperText?: string;
+  errorText?: string;
+} & InputHTMLAttributes<HTMLInputElement>;
+
+const FieldElement: ForwardRefRenderFunction<HTMLInputElement, Props> = (
+  { label, helperText, errorText, name, required, ...rest },
+  ref,
+) => {
+  const hasError = Boolean(errorText);
+
+  return (
+    <S.Container>
+      {label && (
+        <S.LabelContainer>
+          <Label htmlFor={name}>{label}</Label>
+
+          {!required && (
+            <Typography.Text size="body3" color="secondary">
+              Opcional
+            </Typography.Text>
+          )}
+        </S.LabelContainer>
+      )}
+
+      <Input ref={ref} id={name} name={name} error={hasError} {...rest} />
+
+      {helperText && (
+        <Typography.Text as="small" color="secondary">
+          {helperText}
+        </Typography.Text>
+      )}
+
+      {errorText && (
+        <Typography.Text as="small" color="pink">
+          {errorText}
+        </Typography.Text>
+      )}
+    </S.Container>
+  );
+};
+
+export const Field = forwardRef(FieldElement);
