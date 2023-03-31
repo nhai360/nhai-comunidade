952c1bc334bec04f4f882c979a03bc3393131de1
Gabriel <oi@gabrielteodoro.com>
2023-03-31 09:03:08 -0300

    feat: add invalidate queries

--- src/client/comments/index.ts
+++ src/client/comments/index.ts
@@ -1,3 +1,9 @@
+import { QueryClient } from "react-query";
+
+export function invalidateCommentsQueries(queryClient: QueryClient) {
+  queryClient.invalidateQueries("comments");
+}
+
 export * from "./useComments";
 export * from "./useCreateComment";
 export * from "./types";
