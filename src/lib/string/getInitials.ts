4c025e6075840fec6c8ca5e722733a13afec580f
Gabriel <oi@gabrielteodoro.com>
2023-04-03 12:26:47 -0300

    fix: getInitails based on firstName and lastName

--- src/lib/string/getInitials.ts
+++ src/lib/string/getInitials.ts
@@ -1,6 +1,13 @@
 export function getInitials(baseString = "") {
-  return baseString
-    .split(" ")
-    .map((n) => n[0])
-    .join("");
+  if (!baseString.includes(" ")) {
+    return baseString;
+  }
+
+  const [firstName] = baseString.split(" ");
+  const lastName = baseString.split(" ").at(-1);
+
+  return [
+    firstName.charAt(0).toUpperCase(),
+    lastName?.charAt(0).toUpperCase(),
+  ].join("");
 }
