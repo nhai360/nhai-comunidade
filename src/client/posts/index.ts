b1c7a4b806bb753b60b9d314be7cb68420517aaf
Gabriel <oi@gabrielteodoro.com>
2023-04-10 14:22:06 -0300

    feat: add useTrendPosts hook

--- src/client/posts/index.ts
+++ src/client/posts/index.ts
@@ -2,10 +2,12 @@ import { QueryClient } from "react-query";
 
 export function invalidatePostsQueries(queryClient: QueryClient) {
   queryClient.invalidateQueries("posts");
+  queryClient.invalidateQueries("trendPosts");
 }
 
 export * from "./usePost";
 export * from "./usePosts";
+export * from "./useTrendPosts";
 export * from "./useLikePost";
 export * from "./useDeletePost";
 export * from "./useCreatePost";
--- src/features/feed/MainTrends/MainTrends.tsx
+++ src/features/feed/MainTrends/MainTrends.tsx
@@ -2,6 +2,8 @@ import { Avatar, Button, Card, Typography } from "@/ui";
 import { ShareSquareIcon } from "@/ui/_icons";
 
 import { theme } from "@/../stitches.config";
+import { useTrendPosts } from "@/client/posts";
+import { getFirstNameAndLastName, getInitials } from "@/lib/string";
 
 import { FilterButton } from "./FilterButton";
 import { HelpButton } from "./HelpButton";
@@ -9,6 +11,8 @@ import { HelpButton } from "./HelpButton";
 import * as S from "./MainTrends.styles";
 
 export function MainTrends() {
+  const { trendPosts } = useTrendPosts();
+
   return (
     <Card>
       <S.Header>
@@ -21,63 +25,26 @@ export function MainTrends() {
         </S.Actions>
       </S.Header>
       <S.TrendList>
-        <S.TrendItem>
-          <Avatar.Square
-            src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
-            alt="Colm Tuite"
-            fallback="CT"
-          />
-          <S.TrendItemContent>
-            <S.TagFromStatus>Opinião</S.TagFromStatus>
-            <Typography.Text size="body3" color="title" weight="bold">
-              Como construir uma empresa...
-            </Typography.Text>
-            <Typography.Text size="caption" color="secondary">
-              Carlos Alberto
-            </Typography.Text>
-          </S.TrendItemContent>
-          <Button icon size="small" variant="transparent">
-            <ShareSquareIcon color={theme.colors.textSecondary.value} />
-          </Button>
-        </S.TrendItem>
-        <S.TrendItem>
-          <Avatar.Square
-            src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
-            alt="Colm Tuite"
-            fallback="CT"
-          />
-          <S.TrendItemContent>
-            <S.TagFromStatus variant="green">Discussão</S.TagFromStatus>
-            <Typography.Text size="body3" color="title" weight="bold">
-              Como construir uma empresa...
-            </Typography.Text>
-            <Typography.Text size="caption" color="secondary">
-              Carlos Alberto
-            </Typography.Text>
-          </S.TrendItemContent>
-          <Button icon size="small" variant="transparent">
-            <ShareSquareIcon color={theme.colors.textSecondary.value} />
-          </Button>
-        </S.TrendItem>
-        <S.TrendItem>
-          <Avatar.Square
-            src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
-            alt="Colm Tuite"
-            fallback="CT"
-          />
-          <S.TrendItemContent>
-            <S.TagFromStatus variant="yellow">Enquete</S.TagFromStatus>
-            <Typography.Text size="body3" color="title" weight="bold">
-              Como construir uma empresa...
-            </Typography.Text>
-            <Typography.Text size="caption" color="secondary">
-              Carlos Alberto
-            </Typography.Text>
-          </S.TrendItemContent>
-          <Button icon size="small" variant="transparent">
-            <ShareSquareIcon color={theme.colors.textSecondary.value} />
-          </Button>
-        </S.TrendItem>
+        {trendPosts.map((post) => (
+          <S.TrendItem key={post.id}>
+            <Avatar.Square
+              alt={post.author.fullName}
+              fallback={getInitials(post.author.fullName)}
+            />
+            <S.TrendItemContent>
+              <S.TagFromStatus>Opinião</S.TagFromStatus>
+              <Typography.Text size="body3" color="title" weight="bold">
+                Como construir uma empresa...
+              </Typography.Text>
+              <Typography.Text size="caption" color="secondary">
+                {getFirstNameAndLastName(post.author.fullName)}
+              </Typography.Text>
+            </S.TrendItemContent>
+            <Button icon size="small" variant="transparent">
+              <ShareSquareIcon color={theme.colors.textSecondary.value} />
+            </Button>
+          </S.TrendItem>
+        ))}
       </S.TrendList>
     </Card>
   );
