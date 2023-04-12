9ed656cd84ac156e62163fb2a7e4fdbf756d187a
Gabriel <oi@gabrielteodoro.com>
2023-04-12 17:12:33 -0300

    feat: add header component to version app

---
+++ src/layouts/app/DefaultLayout/Header/Header.styles.ts
@@ -0,0 +1,17 @@
+import { styled } from "@/../stitches.config";
+
+export const Container = styled("header", {
+  position: "sticky",
+  top: 0,
+
+  background: "$neutral100",
+
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+
+  borderBottom: "1px solid $neutral200",
+  zIndex: "$fixed",
+
+  padding: "$4 $6",
+});
---
+++ src/layouts/app/DefaultLayout/Header/Header.tsx
@@ -0,0 +1,62 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+import { useAuthContext } from "@/contexts";
+import { Avatar, Button, Logo, Popover, Typography } from "@/ui";
+import { SettingsIcon } from "@/ui/_icons";
+
+import { useUser } from "@/client/users";
+import { authenticatedAPI } from "@/client";
+import { getInitials } from "@/lib/string";
+
+import * as S from "./Header.styles";
+
+export function Header() {
+  const router = useRouter();
+  const { session, logout } = useAuthContext();
+
+  const { user } = useUser({
+    id: session?.userId,
+  });
+
+  function handleLogout() {
+    logout();
+
+    authenticatedAPI.defaults.headers.Authorization = null;
+
+    router.push("/auth/login");
+  }
+
+  return (
+    <S.Container>
+      {user && (
+        <Avatar
+          size="small"
+          progressBar
+          alt={user?.fullName}
+          fallback={getInitials(user.fullName)}
+        />
+      )}
+
+      <Link href="/">
+        <Logo variant="rainbow" />
+      </Link>
+
+      <Popover.Root>
+        <Popover.Trigger asChild>
+          <div>
+            <Button icon variant="transparent">
+              <SettingsIcon />
+            </Button>
+          </div>
+        </Popover.Trigger>
+
+        <Popover side="bottom" sideOffset={8} align="center">
+          <Popover.Action onClick={handleLogout}>
+            <Typography.Text>Sair</Typography.Text>
+          </Popover.Action>
+        </Popover>
+      </Popover.Root>
+    </S.Container>
+  );
+}
