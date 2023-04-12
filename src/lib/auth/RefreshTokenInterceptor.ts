fc862f18d07d0653936ba9779061f73d1e20805e
Gabriel <oi@gabrielteodoro.com>
2023-04-12 11:28:59 -0300

    debug refresh token

--- src/lib/auth/RefreshTokenInterceptor.ts
+++ src/lib/auth/RefreshTokenInterceptor.ts
@@ -6,7 +6,7 @@ import { getSession } from "./getSession";
 import { logout } from "./logout";
 
 export async function refreshTokenInterceptor(error: AxiosError) {
-  console.log("error", error.response);
+  console.log("error", error);
 
   if (error?.response?.status !== 401) {
     return Promise.reject(error);
