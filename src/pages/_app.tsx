4d8a5ebd04492179064642d726ed96f79ed54752
Gabriel <oi@gabrielteodoro.com>
2023-04-19 20:15:42 -0300

    test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from "next/app";
-import { QueryCache, QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { ToastContainer } from "react-toastify";
 import Head from "next/head";
 
@@ -9,9 +9,7 @@ import { AuthProvider } from "@/contexts";
 
 import "react-toastify/dist/ReactToastify.css";
 
-const queryClient = new QueryClient({
-  queryCache: new QueryCache(),
-});
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   globalStyles();
