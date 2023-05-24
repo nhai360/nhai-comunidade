553496cefe9de12c59862c9727e07acabb4b238e
Eder Rosa <eder_junior07@hotmail.com>
2023-05-24 15:56:43 -0300

    feat: card article

--- src/features/articles/CardArticle/index.tsx
+++ src/features/articles/CardArticle/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import styles from "./styles.module.scss";
 import Image from "next/image";
+import { Button } from "@/ui";
 
 const CardArticle = () => {
   return (
@@ -13,15 +14,45 @@ const CardArticle = () => {
           width={0}
           height={0}
           sizes="100vw"
-          style={{ width: "100%", height: 400, objectFit: "cover" }}
+          className={styles.articleImg}
         />
 
-        <div></div>
+        <div className={styles.articleOwnerContainer}>
+          <Image
+            src="https://images.unsplash.com/photo-1682686578456-69ae00b0ecbd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
+            alt=""
+            width={0}
+            height={0}
+            sizes="100vw"
+            className={styles.articleOwner}
+          />
+
+          <div className={styles.articleOwnerInfo}>
+            <h4>
+              Raquel Virgínia{" "}
+              <span
+                className={styles.userRole}
+                style={{ backgroundColor: "#D9F2FF", color: "#01A1FF" }}
+              >
+                ADMIN
+              </span>{" "}
+              <span>23 Jan</span>
+            </h4>
+            <span>Nhaí</span>
+          </div>
+        </div>
 
         <h3>
           Explorando o potencial da colaboração em uma comunidade diversificada
         </h3>
-        <p></p>
+        <p>
+          Experimente o poder da colaboração entre indivíduos de diferentes
+          histórias e ideias inovadoras enquanto faz parte de nossa comunidade.
+        </p>
+
+        <Button className={styles.articleButton}>
+          <h3>Ler artigo completo</h3>
+        </Button>
       </div>
     </>
   );
