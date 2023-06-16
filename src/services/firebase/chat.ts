cc5d8b4821b7cf7218e3f5f5a1303444bbe5e1b1
Gustavo Ferreira <gustavo.rocha.191066@hotmail.com>
2023-06-15 22:13:15 -0300

    feat: chat with firebase

---
+++ src/services/firebase/chat.ts
@@ -0,0 +1,53 @@
+import {
+  addDoc,
+  collection,
+  doc,
+  onSnapshot,
+  serverTimestamp,
+} from "firebase/firestore";
+import { toast } from "react-toastify";
+import { db } from "../firebase";
+
+export const handleGetChat = async (liveId: string, setChat: any) => {
+  try {
+    console.log("[CHAT]: handleGetChat");
+    const liveChatDoc = doc(db, "LIVECHAT", liveId);
+
+    const messageCol = collection(liveChatDoc, "MESSAGES");
+    return onSnapshot(messageCol, (snapshot) => {
+      const messageList = snapshot.docs.map((doc) => {
+        return { ...doc.data() };
+      });
+      setChat(messageList);
+    });
+  } catch (error: any) {
+    toast.error("Falha ao pegar chat: " + error.message);
+    return [];
+  }
+};
+
+export const handleCreateChatMessage = async (
+  liveId: string,
+  chatmessage: ICreateChatMessage
+) => {
+  try {
+    const liveChatDocRef = doc(db, "LIVECHAT", liveId); // Substitua "liveChatId" pelo ID do documento desejado
+    const messageColRef = collection(liveChatDocRef, "MESSAGES");
+
+    const newMessage = {
+      ...chatmessage,
+      createdAt: serverTimestamp(),
+    };
+
+    await addDoc(messageColRef, newMessage);
+  } catch (error: any) {
+    console.error("Falha ao criar novo arquivo:", error);
+    toast.error("Falha ao criar novo arquivo: " + error.message);
+  }
+};
+
+interface ICreateChatMessage {
+  userId: string;
+  userName: string;
+  message: string;
+}
