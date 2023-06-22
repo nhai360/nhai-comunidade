4a86ca7eaea991071a765547df34c36ad9e75c40
Eder Rosa <eder_junior07@hotmail.com>
2023-06-22 19:31:45 -0300

    feat(container hightlight at feed)

--- src/layouts/desktop/DefaultLayout/DefaultLayout.tsx
+++ src/layouts/desktop/DefaultLayout/DefaultLayout.tsx
@@ -4,6 +4,9 @@ import { Header } from "./Header";
 import { Sidebar } from "./Sidebar";
 
 import * as S from "./DefaultLayout.styles";
+import styles from "./defaultLayout.module.scss";
+import { Button } from "@/ui";
+import { PencilSimpleLine } from "@phosphor-icons/react";
 
 type Props = {
   children: ReactNode;
@@ -13,6 +16,15 @@ type Props = {
 export function DefaultLayout({ children, hasSider = true }: Props) {
   return (
     <S.Wrapper>
+      <div className={styles.containerHightlight}>
+        <h4>
+          • 6 Edição do Contaí! Encontro que geram negócios!{" "}
+          <span>| 70% das vagas preenchidas, esperamos por vocês!</span>{" "}
+        </h4>
+        <Button variant={"primary"} style={{ height: 48, borderRadius: 8 }}>
+          Cadastrar <PencilSimpleLine size={24} />
+        </Button>
+      </div>
       <Header />
       {hasSider && <Sidebar />}
       {children}
