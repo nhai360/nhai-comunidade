ed70d345bb07df2a85ea6a8ee7c7e8cb1de8bf6d
Gustavo Ferreira <gustavo.rocha.191066@hotmail.com>
2023-06-26 18:13:53 -0300

    fix: live player

--- src/features/videos/AppLayout/AppLayout.tsx
+++ src/features/videos/AppLayout/AppLayout.tsx
@@ -3,14 +3,26 @@ import { VideoCard } from "../VideoCard";
 import { useVideoContext } from "@/contexts/VideoContext";
 import { Typography } from "@/ui";
 import { FeaturedVideoCard } from "../FeaturedVideoCard";
+import { useLiveContext } from "@/contexts/LiveContext";
+import { LiveWatchCard } from "@/features/lives/LiveWatchCard";
 
 export function AppLayout() {
   const { videos } = useVideoContext();
+  const { lives } = useLiveContext();
   return (
     <DefaultLayout>
       <DefaultLayout.Header />
       <DefaultLayout.Content>
         <FeaturedVideoCard isMobile />
+        <Typography.Text
+          size="h3"
+          style={{ textAlign: "center", margin: "32px 0 8px" }}
+        >
+          Lives para assistir
+        </Typography.Text>
+        {lives.map((live) => (
+          <LiveWatchCard key={live.id} live={live} />
+        ))}
         <Typography.Text
           size="h3"
           style={{ textAlign: "center", margin: "32px 0 8px" }}
