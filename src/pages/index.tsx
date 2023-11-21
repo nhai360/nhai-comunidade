fa42e7c166d32ea7564e251247c05db9e9c8ff1d
GustavoBadaro <gustavo.alves@badaro.design>
2023-11-21 13:40:31 -0300

    fix/error no content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
 import { Feed } from "@/features/feed";
-import { FeedProvider } from "@/contexts";
 import { withAuth } from "../middlewares";
+import { FeedProvider, useAuthContext } from "@/contexts";
+import { useUser } from "@/client/users";
+import { useRouter } from "next/router";
 
 function FeedPage() {
+  const router = useRouter();
+
+  const { session } = useAuthContext();
+
+  if (!session?.userId) {
+    router.push('/auth/login');
+  }
+
   return (
     <FeedProvider>
       <Feed.DesktopLayout />
